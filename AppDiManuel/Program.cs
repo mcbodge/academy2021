bf1168a0bf6079b37934545ade0b09f2fc6d5c39
Manuel <teacher21q1@academy-vm-9>
2021-05-07 16:02:53 +0000

    Add code to Program

--- AppDiManuel/Program.cs
+++ AppDiManuel/Program.cs
@@ -6,7 +6,41 @@ namespace AppDiManuel
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x = 360;
+            x = 10 * 60;
+            int y = 3500 / 10;
+
+            string myMessage = "Riprendiamo alle 17:10";
+
+            string myUpperMessage = myMessage.ToUpper();
+
+            bool isFriday = true;
+            bool isSunday = false;
+
+            if (isSunday)
+            {
+                Console.WriteLine("Is sunday!");
+            }
+
+            Console.WriteLine(myUpperMessage);
+            Console.ReadKey();
+        }
+    }
+
+    public class Human
+    {
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        public void SayHi()
+        {
+            Console.WriteLine(GetMessage());
+        }
+
+        public string GetMessage()
+        {
+            return "Hi, I'm " + Name + "!";
         }
     }
 }
