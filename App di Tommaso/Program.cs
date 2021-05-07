47341554766ab3b00cebd78b741357ea8d8afb75
Tommaso Totaro <academy21q1-7@academy-vm-7>
2021-05-07 14:54:44 +0000

    Add app di tommaso

---
+++ App di Tommaso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace App_di_Tommaso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
